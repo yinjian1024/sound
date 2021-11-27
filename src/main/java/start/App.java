d56559e3a5172f40b7777262f3177532689e2ff7
yinjian <yinjian1024@aliyun.com>
2021-11-27 20:43:56 +0800

    3

--- src/main/java/start/App.java
+++ src/main/java/start/App.java
@@ -3,7 +3,7 @@ package start;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages="com")
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
